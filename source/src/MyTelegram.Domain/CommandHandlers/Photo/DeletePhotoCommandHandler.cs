6990371e42905d44279d66a6e8604eb9eea09d13
mytelegram <mytelegramserver@outlook.com>
2025-03-28 11:49:58 +0800

    feat: DeletePhotosHandler

---
+++ source/src/MyTelegram.Domain/CommandHandlers/Photo/DeletePhotoCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace MyTelegram.Domain.CommandHandlers.Photo;
+
+public class DeletePhotoCommandHandler : CommandHandler<PhotoAggregate, PhotoId, DeletePhotoCommand>
+{
+    public override Task ExecuteAsync(PhotoAggregate aggregate,
+        DeletePhotoCommand command,
+        CancellationToken cancellationToken)
+    {
+        aggregate.Delete(command.UserId);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ source/src/MyTelegram.Domain/Commands/Photo/DeletePhotoCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace MyTelegram.Domain.Commands.Photo;
+
+public class DeletePhotoCommand(PhotoId aggregateId, long userId/*, long accessHash*/)
+    : Command<PhotoAggregate, PhotoId, IExecutionResult>(aggregateId)
+{
+    public long UserId { get; } = userId;
+    //public long AccessHash { get; } = accessHash;
+}
