9a7a05f832a356f072ae291f0cb0f2c248dff1d2
mytelegram <mytelegramserver@outlook.com>
2025-04-15 22:32:22 +0800

    Add a warning message when using the default private key

--- source/src/MyTelegram.AuthServer/BackgroundServices/MyTelegramAuthServerBackgroundService.cs
+++ source/src/MyTelegram.AuthServer/BackgroundServices/MyTelegramAuthServerBackgroundService.cs
@@ -2,13 +2,20 @@
 
 public class MyTelegramAuthServerBackgroundService(
     ILogger<MyTelegramAuthServerBackgroundService> logger,
-    IHandlerHelper handlerHelper
+    IHandlerHelper handlerHelper,
+    IFingerprintHelper fingerprintHelper
 ) : BackgroundService
 {
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         handlerHelper.InitAllHandlers();
         logger.LogInformation("MyTelegram auth server started");
+        const long defaultFingerprint = -3591632762792723036;
+        var fingerprint = fingerprintHelper.GetFingerprint();
+        if (fingerprint == defaultFingerprint)
+        {
+            logger.LogWarning("You are currently using the default private key, which anyone can obtain from the mytelegram open source project. For security reasons, please use your own private key and replace the client's public key.");
+        }
 
         return Task.CompletedTask;
     }
